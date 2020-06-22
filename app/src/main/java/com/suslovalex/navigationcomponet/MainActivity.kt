d77684b149fbc5995774c6d856c3b5cb2def381c
AleksandrAlex <suslovalmi@mail.ru>
2020-06-22 21:15:16 +0300

    Navigation Drawer

--- app/src/main/java/com/suslovalex/navigationcomponet/MainActivity.kt
+++ app/src/main/java/com/suslovalex/navigationcomponet/MainActivity.kt
@@ -3,14 +3,33 @@ package com.suslovalex.navigationcomponet
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.suslovalex.navigationcomponet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+        navController = findNavController(R.id.fragment)
+        drawerLayout = binding.drawerLayout
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+        NavigationUI.setupWithNavController(binding.navigationView,navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
